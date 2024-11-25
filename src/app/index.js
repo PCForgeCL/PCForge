b80588d0b34233a9c657797d3736ba3cbd6a5789
0wulf <francisco.wulf@uc.cl>
2024-11-25 16:36:17 -0300

    +: cors

--- src/app/index.js
+++ src/app/index.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const { Pool } = require('pg');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 // PostgreSQL connection setup using environment variables
